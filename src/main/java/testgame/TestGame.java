056cae34e9423190a223dfeb6e4a4ef409eeaebc
Izaak Baker <ibaker@palantir.com>
2016-12-29 22:26:33 -0500

    Add comments

--- src/main/java/testgame/TestGame.java
+++ src/main/java/testgame/TestGame.java
@@ -9,13 +9,22 @@ import xyz.izaak.radon.math.Points;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
 
 public class TestGame {
-    public static final Vector2i WINDOW_SIZE = new Vector2i(1200, 800);
+    static final Vector2i WINDOW_SIZE = new Vector2i(1200, 800);
 
     public static void main(String... args) {
+        // Create a new game with a 1200x800 window
         Game testGame = new Game("TestGame", WINDOW_SIZE.x, WINDOW_SIZE.y, Points.BLACK);
+
+        // Game should exit when player presses ESC
         testGame.addGameSystem(new KeyReleaseQuitSystem(testGame.getWindow(), GLFW_KEY_ESCAPE));
+
+        // Camera motion uses WASD (move) + LSHIFT (down) + SPACE (up) + mouse (look around)
         testGame.addGameSystem(new FlyingCameraSystem(Points.__Z));
+
+        // Add TestGame custom world builder system which will set up demo scenes
         testGame.addGameSystem(new WorldBuilderSystem());
+
+        // And we're off!
         testGame.run();
     }
 }
--- src/main/java/testgame/WorldBuilderSystem.java
+++ src/main/java/testgame/WorldBuilderSystem.java
@@ -26,10 +26,7 @@ import xyz.izaak.radon.world.Scene;
 
 import static testgame.TestGame.WINDOW_SIZE;
 
-/**
- * Created by ibaker on 29/12/2016.
- */
-public class WorldBuilderSystem implements GameSystem {
+class WorldBuilderSystem implements GameSystem {
     private Scene currentScene;
     private Camera camera;
     private Channel<Camera> currentCameraChannel;
@@ -37,9 +34,11 @@ public class WorldBuilderSystem implements GameSystem {
 
     @Override
     public void initialize() {
+        // Request Radon builtin Camera and Scene channels, used by FlyingCameraSystem
         currentCameraChannel = Channel.request(Camera.class, Channel.CURRENT_CAMERA);
         currentSceneChannel = Channel.request(Scene.class, Channel.CURRENT_SCENE);
 
+        // Set up and publish Camera object so that the FlyingCameraSystem can move it around
         float aspectRatio = (float) (WINDOW_SIZE.x) / (float) WINDOW_SIZE.y;
         float fov = Points.piOver(3);
         Vector3f eye = new Vector3f(9f, 9f, 4f);
@@ -48,12 +47,13 @@ public class WorldBuilderSystem implements GameSystem {
         camera = Camera.builder().aspectRatio(aspectRatio).fov(fov).eye(eye).look(look).up(up).build();
         currentCameraChannel.publish(camera);
 
+        // Create two Scene objects and link them by their Portals
         Scene spheresScene = buildSpheresScene();
         Scene isosurfaceScene = buildIsosurfaceScene();
-
         spheresScene.getPortals().get(0).link(isosurfaceScene.getPortals().get(0));
         isosurfaceScene.getPortals().get(0).link(spheresScene.getPortals().get(0));
 
+        // Publish the initial scene, and subscribe to changes
         currentSceneChannel.publish(isosurfaceScene);
         currentSceneChannel.subscribe(scene -> this.currentScene = scene);
     }
@@ -70,6 +70,7 @@ public class WorldBuilderSystem implements GameSystem {
     }
 
     private Scene buildSpheresScene() {
+        // Create floor entity
         Mesh floorFill = new Mesh(new QuadGeometry(), new SolidColorMaterial(Points.GRAY));
         floorFill.scale(10f, 10f, 1f);
         Mesh floorOutline = new Mesh(new QuadOutlineGeometry(), new SolidColorMaterial(Points.WHITE));
@@ -77,6 +78,7 @@ public class WorldBuilderSystem implements GameSystem {
         Entity floor = Entity.builder().build();
         floor.addMeshes(floorFill, floorOutline);
 
+        // Create solid red sphere entity with white wireframe outline
         Mesh solidSphereFill = new Mesh(new PolarSphereGeometry(15, 15), new SolidColorMaterial(Points.RED));
         Mesh sphereWireframe = new Mesh(new PolarSphereOutlineGeometry(15, 15), new SolidColorMaterial(Points.WHITE));
         sphereWireframe.scale(1.01f);
@@ -84,22 +86,27 @@ public class WorldBuilderSystem implements GameSystem {
         solidSphere.addMeshes(solidSphereFill, sphereWireframe);
         solidSphere.translate(0f, 0f, 2f);
 
+        // Create normal-colored sphere entity
         Mesh normalSphereFill = new Mesh(new PolarSphereGeometry(15, 15), new NormalMaterial());
         Entity normalSphere = Entity.builder().build();
         normalSphere.addMeshes(normalSphereFill);
         normalSphere.translate(0f, 3f, 2f);
 
-        PhongMaterial pinkPlastic = new PhongMaterial(Points.BLUE, Points.BLUE, Points.WHITE, Points.BLACK, 100f);
-        Mesh phongSphereFill = new Mesh(new PolarSphereGeometry(15, 15), pinkPlastic);
+        // Create specular blue sphere entity
+        PhongMaterial bluePlastic = new PhongMaterial(Points.BLUE, Points.BLUE, Points.WHITE, Points.BLACK, 100f);
+        Mesh phongSphereFill = new Mesh(new PolarSphereGeometry(15, 15), bluePlastic);
         Entity phongSphere = Entity.builder().build();
         phongSphere.addMeshes(phongSphereFill);
         phongSphere.translate(0f, -3f, 2f);
 
+        // Create a directional light to illuminate the blue sphere
         DirectionalLight sun = new DirectionalLight(Points.WHITE, Points.XYZ);
 
+        // Create a portal to add to the scene
         OrthonormalBasis portalBasis = new OrthonormalBasis(Points._Y_, Points.__Z);
         Portal portal = new Portal(new Vector3f(-3f, 0f, Portal.PORTAL_DIMENSIONS.y / 2), portalBasis);
 
+        // Compose and return a Scene containing the elements created
         Scene spheresScene = Scene.builder().build();
         spheresScene.addEntity(floor);
         spheresScene.addEntity(solidSphere);
@@ -107,38 +114,41 @@ public class WorldBuilderSystem implements GameSystem {
         spheresScene.addEntity(phongSphere);
         spheresScene.addDirectionalLight(sun);
         spheresScene.addPortal(portal);
-
         return spheresScene;
     }
 
     private Scene buildIsosurfaceScene() {
+        // Set up parameters for extracting an IsosurfaceGeometry over a 20x20x20 region of the Scene
         int sampleDensity = 40;
         float worldSize = 20f;
         Vector3f min = new Vector3f(-worldSize / 2f);
         Vector3i dimensions = new Vector3i(sampleDensity);
         float fidelity = worldSize / (float) sampleDensity;
 
+        // Create a bumpy function and extract a surface at value 0 into an IsosurfaceGeometry
         ScalarVolume function = (x, y, z) -> (float) (Math.sin(x / 2) * Math.sin(y / 2) - z);
         IsosurfaceGeometry geometry = new IsosurfaceGeometry(function, min, dimensions, 0f, fidelity);
 
+        // Create an entity which uses the extracted surface geometry
         PhongMaterial material = new PhongMaterial(Points.WHITE, Points.WHITE, Points.WHITE, Points.BLACK, 10f);
-
         Mesh isosurfaceMesh = new Mesh(geometry, material);
         Entity isosurfaceEntity = Entity.builder().build();
         isosurfaceEntity.addMeshes(isosurfaceMesh);
 
+        // Create some light objects to illuminate the scene
         PointLight green = new PointLight(new Vector3f(0f, 5f, 0f), new Vector3f(-2.5f, 2.5f, 3f));
         PointLight white = new PointLight(new Vector3f(3f, 3f, 3f), new Vector3f(2.5f, -2.5f, 3f));
 
+        // Create a Portal to add to the scene
         OrthonormalBasis portalBasis = new OrthonormalBasis(Points.x__, Points.__Z);
         Portal portal = new Portal(new Vector3f(3f, 0f, Portal.PORTAL_DIMENSIONS.y / 2), portalBasis);
 
+        // Compose and return a Scene containing the elements created
         Scene isosurfaceScene = Scene.builder().build();
         isosurfaceScene.addEntity(isosurfaceEntity);
         isosurfaceScene.addPointLight(green);
         isosurfaceScene.addPointLight(white);
         isosurfaceScene.addPortal(portal);
-
         return isosurfaceScene;
     }
 }

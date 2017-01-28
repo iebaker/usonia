065a7490e92445af7ec474ea25b157f8d569a22b
Izaak Baker <ibaker@palantir.com>
2017-01-27 18:27:18 -0800

    Rickety

--- src/main/java/usonia/Application.java
+++ src/main/java/usonia/Application.java
@@ -1,12 +1,15 @@
 package usonia;
 
 import xyz.izaak.radon.external.xml.XmlSceneLoader;
+import xyz.izaak.radon.scene.Scene;
 
 public class Application {
     public static void main(String... args) {
         System.out.println("hey");
 
         XmlSceneLoader testSceneLoader = new XmlSceneLoader("testScene.xml");
-        testSceneLoader.newInstance();
+        Scene test = testSceneLoader.newInstance();
+
+        System.out.println(test.getNode("root"));
     }
 }
